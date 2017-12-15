d7fca2c86a4372ecdd711758b37ece533d2eb73a
Ruben Diaz <ruben.diaz-martinez@capgemini.com>
2017-12-15 14:54:40 +0100

    cleaned

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { LayoutModule } from './layout/layout.module';
 import { HomeModule } from './home/home.module';
-import { SampleDataModule } from './sample-data/sample-data.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -22,8 +21,7 @@ import { LoginComponent } from './login/login.component';
     LayoutModule,
     AppRoutingModule,
     CoreModule,
-    HomeModule,
-    SampleDataModule
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
