d02202ff5e2b10ec58e3207e90d83933789460ad
Santos Jiménez <santos.jimenez-linares@capgemini.com>
2018-12-18 16:09:11 +0100

    Template upgraded to Angular 7.1.3 and Covalent RC2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment: {
   security: 'csrf' | 'jwt';
 } = {
   production: false,
-  restPathRoot: 'http://localhost:8081/demo-server/',
-  restServiceRoot: 'http://localhost:8081/demo-server/services/rest/',
+  restPathRoot: 'http://localhost:8081/',
+  restServiceRoot: 'http://localhost:8081/services/rest/',
   security: 'csrf',
 };
