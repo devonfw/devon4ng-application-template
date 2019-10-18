67d2acc24635b9479057339caa6cdec4f7d343cf
Santos Jiménez <santos.jimenez-linares@capgemini.com>
2019-10-18 10:29:28 +0200

    ng update and JWT enabled by default

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,5 +7,5 @@ export const environment: {
   production: false,
   restPathRoot: 'http://localhost:8081/demo-server/',
   restServiceRoot: 'http://localhost:8081/demo-server/services/rest/',
-  security: 'csrf',
+  security: 'jwt',
 };
